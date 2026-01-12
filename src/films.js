6a7ba1f67dde53cf0c051d51cb45e251169aa3d2
Carlos Ramirez <crbarrio@hotmail.com>
2026-01-12 13:12:38 +0100

    ejercicios resueltos

--- src/films.js
+++ src/films.js
@@ -1,43 +1,89 @@
+
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result =  array.map( movie => movie.director );
+
   return result;
 }
 
+
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter( movie => movie.director === director)
+
+  return result;
 }
+
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let movies = getMoviesFromDirector(array, director)
+  let suma = movies.reduce((total, movie) => {
+    return total + movie.score
+  },0);
+
+  let avg = parseFloat((suma / movies.length).toFixed(2));
+
+  return avg;
 }
+
+
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let result = []
+  array.toSorted((a,b) => (a.title.localeCompare(b.title))).forEach(movie => {
+    result.push (movie.title)
+  });
+  result = result.slice(0,20)
+
+  return result;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
+
+  return array.toSorted((a, b) => (a.year - b.year || a.title.localeCompare(b.title)))
 
 }
+
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
+function moviesAverageByCategory(array, category) {
+  let movies = array.filter(movie => movie.genre.find(e => e == category))
+  let suma = movies.reduce((total, movie) => {
+    return total + movie.score
+  }, 0)
+  return parseFloat((suma / movies.length).toFixed(2))
 }
 
-// Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
 
+// Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes(array) {
+  let result = []
+
+  array.forEach(movie => {
+    let finHora = movie.duration.indexOf('h')
+    let inicioMin = movie.duration.indexOf(' ')
+    let finMin = movie.duration.indexOf('min')
+    let hora = finHora != -1 ? parseInt(movie.duration.slice(0, finHora)) * 60 : 0
+    let min = finMin != -1 ?  parseInt(movie.duration.slice(inicioMin, finMin)) : 0
+    let duracion = hora + min
+    let newMovie ={...movie}
+    newMovie.duration = duracion
+    result.push(newMovie)
+    
+  })
+
+  return result
 }
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
+function bestFilmOfYear(array, year) {
+  let result = []
+  let movies = array.filter(movie => movie.year == year)
+  let maxScore = movies.toSorted((a, b) => (b.score - a.score))[0].score
+  return movies.filter(movie => movie.score == maxScore)
 }
 
 
--- tests/films.spec.js
+++ tests/films.spec.js
@@ -293,9 +293,63 @@ describe('Function "orderByYear"', () => {
 // Exercise 6
 // YOUR CODE HERE. Test moviesAverageByCategory()
 describe('Function "moviesAverageByCategory"', () => {
-  it('ADD YOUR CODE IN films.spec.js file', () => {
-    expect(typeof hoursToMinutes).toBe('coffee');
+  it('should be declared', () => {
+    expect(typeof moviesAverageByCategory).toBe('function');
   });
+
+  it('should return a number', () => {
+    expect(typeof moviesAverageByCategory(movies, 'Crime')).toBe('number');
+  });
+
+  it('should be different from NaN', () => {
+    expect(moviesAverageByCategory(movies, 'Crime')).not.toBeNaN();
+  });
+
+  it(' should return the average score of movies selecting only the category films. With 2 decimals! ', () => {
+    expect(moviesAverageByCategory([
+      {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      duration: '2h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9.3
+    },
+    {
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola',
+      duration: '2h 55min',
+      genre: ['Crime', 'Drama'],
+      score: 9.2
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola',
+      duration: '3h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9
+    },
+    {
+      title: 'The Dark Knight',
+      year: 2008,
+      director: 'Christopher Nolan',
+      duration: '2h 32min',
+      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+      score: 9
+    },
+    {
+      title: '12 Angry Men',
+      year: 1957,
+      director: 'Sidney Lumet',
+      duration: '1h 36min',
+      genre: ['Crime', 'Drama'],
+      score: 8.9
+    }
+    ], 'Crime')).toBe(9.08);
+  });
+
 });
 
 // Exercise 7
